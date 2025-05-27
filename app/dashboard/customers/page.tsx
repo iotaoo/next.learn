e049bfdf5954f905a6c72e5d5e857ce2c8baff69
iota <hero5@vip.qq.com>
2025-05-27 13:13:40 +0800

    next.learn

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const Customers = () => {
+  return <div>customers content</div>;
+};
+
+export default Customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const Invoices = () => {
+  return <div>invoices content</div>;
+};
+
+export default Invoices;
